eded155be25f6dcb44556e936a3c88b0e4c8022b
Rishabh471 <64025471+Rishabh471@users.noreply.github.com>
2022-01-15 17:06:30 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+export const auth = firebase.initializeApp ({
+    apiKey: "AIzaSyD4cdqA8v-qKp98QGwzoeOlUbHoMXZc2Nc",
+    authDomain: "vaarta-f9233.firebaseapp.com",
+    projectId: "vaarta-f9233",
+    storageBucket: "vaarta-f9233.appspot.com",
+    messagingSenderId: "499576716792",
+    appId: "1:499576716792:web:cae1b4302f9af8d9312a0d"
+  }).auth();
+
+
+  // me-"405195b9-4679-4b0f-87d3-93c07683b2cc",
+  //users-"3461357e-9de4-484b-bc7c-a8ac7e9f0222"
+  //pid-"405195b9-4679-4b0f-87d3-93c07683b2cc"
